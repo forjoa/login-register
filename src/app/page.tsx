108939934b34d38df9a918d62c91e8ad8f18d180
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-02-14 13:05:04 +0100

    add options between login and register

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
-import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
-  return <></>
+  return (
+    <main className='mx-auto my-10 w-1/2'>
+      <h1>Select an option:</h1>
+      <div className='options-container flex gap-5'>
+        <Link
+          href='/login'
+          className='bg-blue-500 text-white py-2 px-4 rounded-md'
+        >
+          Login
+        </Link>
+        <Link
+          href='/register'
+          className='bg-slate-300 text-black py-2 px-4 rounded-md'
+        >
+          Register
+        </Link>
+      </div>
+    </main>
+  )
 }
