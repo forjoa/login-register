47138cf764461ef399a70a0e278dbce2d66b039b
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-02-15 09:02:06 +0100

    change layout to return to every page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,8 @@
-import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className='mx-auto my-10 w-1/2'>
-      <h1>Select an option:</h1>
-      <div className='options-container flex gap-5'>
-        <Link
-          href='/login'
-          className='bg-blue-500 text-white py-2 px-4 rounded-md'
-        >
-          Login
-        </Link>
-        <Link
-          href='/register'
-          className='bg-slate-300 text-black py-2 px-4 rounded-md'
-        >
-          Register
-        </Link>
-      </div>
+    <main className='mx-auto w-1/2'>
+      <h1>Welcome</h1>
     </main>
   )
 }
