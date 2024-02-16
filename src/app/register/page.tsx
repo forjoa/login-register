c517c2093928a17e18e00b8cf4f9a144925cb161
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-02-16 21:00:16 +0100

    fix: add autocomplete elements

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -11,12 +11,27 @@ const Register = () => {
     <main className='mx-auto w-1/2'>
       <h1>Register</h1>
       <form>
+        <label htmlFor='name'>Full name</label>
+        <br />
+        <input
+          type='text'
+          name='name'
+          id='name'
+          autoComplete='name'
+          spellCheck='false'
+          className='border-2 rounded-md p-2 outline-none'
+        />
+
+        <br />
+        <br />
+
         <label htmlFor='username'>Username</label>
         <br />
         <input
           type='text'
           name='username'
           id='username'
+          autoComplete='username'
           spellCheck='false'
           className='border-2 rounded-md p-2 outline-none'
         />
@@ -44,11 +59,13 @@ const Register = () => {
             type={lock ? 'password' : 'text'}
             name='password'
             id='password'
+            autoComplete='current-password'
             className='border-2 rounded-md p-2 outline-none'
           />
           <Image
             src={lock ? lockI : unlock}
             alt='Locked or unlocked'
+            className='w-auto h-auto'
             onClick={() => setLock(!lock)}
           ></Image>
         </div>
