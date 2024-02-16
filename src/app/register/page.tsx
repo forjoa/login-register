0bb4b1e8568fd937ff1676384275be0b6c3df64b
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-02-16 20:03:48 +0100

    register form layout ok

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,44 @@
+'use client'
+import { useState } from 'react'
+import lockI from '@/assets/lock.svg'
+import unlock from '@/assets/unlock.svg'
+import Image from 'next/image'
+
 const Register = () => {
+  const [lock, setLock] = useState<boolean>(true)
+
   return (
     <main className='mx-auto w-1/2'>
       <h1>Register</h1>
+      <form>
+        <label htmlFor='username'>Username</label>
+        <br />
+        <input type='text' name='username' id='username' spellCheck='false' />
+
+        <br />
+        <br />
+
+        <label htmlFor='email'>Email</label>
+        <br />
+        <input type='email' name='email' id='email' autoComplete='email' />
+
+        <br />
+        <br />
+
+        <label htmlFor='password'>Password</label>
+        <br />
+        <input
+          type={lock ? 'password' : 'text'}
+          name='password'
+          id='password'
+        />
+        <Image src={lock ? lockI : unlock} alt='Locked or unlocked' onClick={() => setLock(!lock)}></Image>
+
+        <br />
+        <br />
+
+        <input type='submit' value='Register' />
+      </form>
     </main>
   )
 }
