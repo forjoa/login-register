52c72e705e85d66c4c0fc84a4ea92ce497d83e5a
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-02-16 22:21:52 +0100

    add some text in welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@ export default function Home() {
   return (
     <main className='mx-auto w-1/2'>
       <h1>Welcome</h1>
+      <p className='text-neutral-500'>
+        This is how I&apos;m learning how to login and register in Next.js with
+        JWT Authentication.
+      </p>
     </main>
   )
 }
